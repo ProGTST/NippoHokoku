e074a52cc6645de99f4cf605ad2d8708f5c4e5ee
tsuchida <tsuchida_spc_0002-at-619888520551>
2026-07-14 17:51:32 +0900

    v0.1.9リリース

--- preload.js
+++ preload.js
@@ -2,7 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('appInfo', {
   name: '日報入力',
-  version: '0.1.8',
+  version: '0.1.9',
   targetUrl: 'https://rkanri.genech.co.jp/kanri/nippo'
 });
 
